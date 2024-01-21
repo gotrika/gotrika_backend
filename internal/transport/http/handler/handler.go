da5e5a803c62588ddb758cacde0b41fc0b88c01b
bzdvdn <bzdv.dn@gmail.com>
2024-01-22 01:13:20 +0300

    add redoc

--- internal/transport/http/handler/handler.go
+++ internal/transport/http/handler/handler.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gotrika/gotrika_backend/internal/config"
 	"github.com/gotrika/gotrika_backend/internal/service"
 	"github.com/gotrika/gotrika_backend/internal/transport/http/handler/api"
+	"github.com/mvrilo/go-redoc"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
@@ -23,6 +24,13 @@ func NewHandler(services *service.Services) *Handler {
 func (h *Handler) Init(cfg *config.Config) *gin.Engine {
 	// Init gin handler
 	router := gin.Default()
+	rdoc := redoc.Redoc{
+		Title:       "Gotrika API",
+		Description: "Gotrika Backend API",
+		SpecFile:    "./docs/swagger.json",
+		SpecPath:    "/docs/doc.json",
+		DocsPath:    "/redoc/",
+	}
 
 	router.Use(
 		gin.Recovery(),
@@ -32,6 +40,11 @@ func (h *Handler) Init(cfg *config.Config) *gin.Engine {
 	router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	router.GET("/ping/", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong")
+	})
+	router.GET("/redoc/", func(ctx *gin.Context) {
+		handler := rdoc.Handler()
+		handler(ctx.Writer, ctx.Request)
+		ctx.Next()
 	})
 	apiHandler := api.NewAPIHandler(h.services)
 	apiHandler.Init(router)
